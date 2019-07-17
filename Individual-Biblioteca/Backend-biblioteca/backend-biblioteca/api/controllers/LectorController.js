e1ee25331a124fb02f13f9ea1df247bad3c486aa
DanyJos3 <Jose196.jm@hotmail.com>
2019-07-17 10:00:33 -0500

    inicio backend

---
+++ Individual-Biblioteca/Backend-biblioteca/backend-biblioteca/api/controllers/LectorController.js
@@ -0,0 +1,12 @@
+/**
+ * LectorController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
